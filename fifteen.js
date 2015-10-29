b9fc58b92070e704553ba09d491a113b028147d5
keezia <keeziallen@gmail.com>
2015-10-29 03:22:40 +0000

    first commit

---
+++ fifteen.js
@@ -0,0 +1,25 @@
+window.onload = function()
+{
+	var puzzlearea = document.getElementById("puzzlearea");
+	var pieces = puzzlearea.children;
+	var positionTop = 0;
+	var positionLeft = 0;
+
+	for(var i =0; i < pieces.length; i++)
+	{
+		pieces[i].className = "puzzlepiece";
+		pieces[i].style.top =  positionTop + "px";
+		pieces[i].style.left = positionLeft + "px";
+
+		if(positionLeft < 300)
+		{
+			positionLeft = positionLeft + 100;
+			
+		}
+		else
+		{
+			positionLeft = 0;
+			positionTop = positionTop + 100;
+		}	
+	}
+}
