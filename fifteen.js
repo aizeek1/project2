9c1698bf47c69b9482a3f99c4990dfb2bc3042d5
keezia <keeziallen@gmail.com>
2015-11-06 02:34:07 +0000

    puzzle pieces now move

--- fifteen.js
+++ fifteen.js
@@ -1,38 +1,102 @@
-window.onload = function()
-{
-	var puzzlearea = document.getElementById("puzzlearea");
-	var pieces = puzzlearea.children;
-	var positionTop = 0;
-	var positionLeft = 0;
-	var backgroundPositionLeft = 0;
-	var backgroundPositionTop=0;
-
-	for(var i =0; i < pieces.length; i++)
-	{
-		pieces[i].className = "puzzlepiece";
-		pieces[i].style.top= positionTop + "px";
-		pieces[i].style.left = positionLeft + "px";
-		pieces[i].style.backgroundPosition= backgroundPositionLeft +"px" + backgroundPositionTop +"px";
-		
-
-		if(positionLeft < 300 )
-		{
-			positionLeft = positionLeft + 100;
-			backgroundPositionLeft = backgroundPositionLeft - 100;
-			
-		}
-		else
-		{
-			positionLeft = 0;
-			backgroundPositionLeft = 0;
-			positionTop = positionTop + 100;
-			backgroundPositionTop = backgroundPositionTop - 100;
-			
-		}	
-	}
-	
-
+"use strict";
+window.onload = function(){
+	startGame();
 };
+	var puzzlepieces;
+	var puzzle = []; // creating an array to hold the puzzle pieces
 
+function fixTiles(piece, position){
+    positionT(piece, position);
+    positionL(piece, position);
+
+    piece.style.backgroundPosition = (400 - getLeft(position)) + "px" + " " + (400 - getTop(position)) + "px";
+}
+
+function layoutPuzzle(){ // creating the layout of the puzzle pieces 
+    puzzlepieces = $$('#puzzlearea div');
+    var counter = 0;
+    puzzlepieces.each(function(element)
+    {
+        element.addClassName('puzzlepiece');
+        element.id = counter;
+        console.log(element.id);
+        fixTiles(element, counter);
+        puzzle[counter] = 1;
+        counter++;
+    });
+    puzzle[15] = 0;
+}
+
+function emptySpace(){
+    for (var tilePos = 0; tilePos < 16; tilePos++){
+       if (puzzle[tilePos] === 0){
+            return tilePos;
+        }
+    }
+}
+
+function tileThatCanMove(tilePos) { //deciding which tiles are movable based on position
+    var emptyTilePos = emptySpace();
+    if ((tilePos % 4 != 0 && tilePos - 1 == emptyTilePos) ||(tilePos % 4 != 3 && tilePos + 1 == emptyTilePos) || (tilePos + 4 == emptyTilePos) || (tilePos - 4 == emptyTilePos)) {
+        return emptyTilePos;
+    }
+    return -1;
+}
+
+function getLeft(position){
+    return (position % 4) * 100;
+}
+
+function getTop(position){
+    return Math.floor(position / 4) * 100;
+}
+function positionL(piece, position){
+    piece.style.left = getLeft(position) + "px";
+}
+
+function positionT(piece, position){
+    piece.style.top = getTop(position) + "px";
+}
+
+function move(piece, toMoveTo){
+    positionT(piece, toMoveTo);
+    positionL(piece, toMoveTo);
+    puzzle[toMoveTo] = 1;
+    console.log(piece.id);
+    console.log(puzzle[piece.id]);
+    puzzle[piece.id] = 0;
+    piece.id = toMoveTo;
+    console.log(piece.id);
+}
+	
+function movePiece(piece){
+    puzzlepieces[piece].onclick = function(){
+        var canMoveTo = tileThatCanMove(parseInt(puzzlepieces[piece].id,10));
+        console.log("INDEX:" + canMoveTo);
+        if (canMoveTo != -1){
+            move(puzzlepieces[piece], canMoveTo);
+        }
+    };
+}
+
+function onMouseHover(k){
+    puzzlepieces[k].onmouseover = function(){
+        var position = tileThatCanMove(parseInt(puzzlepieces[k].id,10));
+        if (position != -1){
+            puzzlepieces[k].addClassName('movablepiece');
+        }
+        else{
+            puzzlepieces[k].removeClassName('movablepiece');
+        }
+    };
+}
+
+    function startGame(){ // this function will be initiated to startthe game so that users can in teract with the puzzle
+    layoutPuzzle();
+    for (var k = 0; k < 15; k++){
+        movePiece(k);
+        onMouseHover(k);
+    }
+ }
 
 
